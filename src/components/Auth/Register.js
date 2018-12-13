ff76d8ec880b27216e28d0fdd64e921daaf688f8
SathishSaminathan <Sathishcse1975@gmail.com>
2018-12-13 12:38:27 +0530

    initial_commit

---
+++ src/components/Auth/Register.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react'
+
+class Reginster extends Component {
+    render () {
+        return (
+            <div>
+                Register
+            </div>
+        )
+    }
+}
+
+export default Reginster
