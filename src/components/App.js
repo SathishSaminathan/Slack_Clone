5d537ed2d7f6ef7b9ee534ce5a098ffc86cbb0ce
SathishSaminathan <Sathishcse1975@gmail.com>
2018-12-15 10:58:34 +0530

    initial_commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
+
+import ColorPanel from "./ColorPanel/ColorPanel";
+import SidePanel from "./SidePanel/SidePanel";
+import Messages from "./Messages/Messages";
+import MetaPanel from "./MetaPanel/MetaPanel";
+
 import "./App.css";
 
-class App extends Component {
-  render() {
-    return <div className="App">App</div>;
-  }
-}
+const GridColumn = Grid.Column;
+
+const App = () => {
+  return (
+    <Grid columns="equal" className="app" style={{ background: "eee" }}>
+      <ColorPanel />
+      <SidePanel />
+      <GridColumn style={{
+        marginLeft:320
+      }}>
+        <Messages />
+      </GridColumn>
+      <GridColumn width={4}>
+        <MetaPanel />
+      </GridColumn>
+    </Grid>
+  );
+};
 
 export default App;
